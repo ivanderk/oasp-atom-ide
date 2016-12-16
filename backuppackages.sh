c804f191cbd3866c1b45c24028f4b1e754984acd
Alfonso Garcia <david.alfonso-garcia@capgemini.com>
2016-12-16 08:46:16 +0100

    Bash fixes

--- backuppackages.sh
+++ backuppackages.sh
@@ -1,9 +1,10 @@
-﻿@echo off
+#!/usr/bin/env bash
 
-pushd %~dp0
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-call env.bat
+pushd $DIR
+source env.sh
 
-%ATOM_BIN%\resources\app\apm\bin\apm  list --installed --bare %ATOM_HOME%\packages.lst
+$ATOM_BIN/resources/app/apm/bin/apm  list --installed --bare | tee $ATOM_HOME/packages.lst 
 
 popd
